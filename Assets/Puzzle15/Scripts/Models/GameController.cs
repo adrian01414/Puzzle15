0376fd4b142ed52f2074ae732caff8402b86b616
Adrian <60646083+adrian01414@users.noreply.github.com>
2025-07-30 12:33:03 +0700

    refact tests

--- Assets/Puzzle15/Scripts/Models/GameController.cs
+++ Assets/Puzzle15/Scripts/Models/GameController.cs
@@ -2,106 +2,111 @@ using System;
 using System.Text;
 using UnityEngine;
 
-public class GameController
+namespace Puzzle15
 {
-    public event Action<int, int, int, int> OnCellSwapped = null;
-    public event Action OnWin = null;
-
-    private int _gridSize = 0;
-    private int[,] _grid  = null;
-
-    public GameController(int gridSize, int[,] grid = null)
+    public class GameController
     {
-        if(grid == null)
+        public event Action<int, int, int, int> OnCellSwapped = null;
+        public event Action OnWin = null;
+
+        private int _gridSize = 0;
+        private int[,] _grid = null;
+
+        public GameController(int gridSize, int[,] grid = null)
         {
-            _grid = new int[gridSize, gridSize];
-            _gridSize = gridSize;
-            InitializeGrid();
-        } else
-        {
-            _grid = (int[,])grid.Clone();
-            _gridSize = gridSize;
-            CheckGridValid();
-        }
-        
-    }
-
-    public void ClickOnCell(int i, int j) {
-        if (i > 0 && _grid[i - 1, j] == 0) { SwapCells(i, j, i - 1, j); }
-        else if (i < _gridSize - 1 && _grid[i + 1, j] == 0) { SwapCells(i, j, i + 1, j); }
-        else if (j > 0 && _grid[i, j - 1] == 0) { SwapCells(i, j, i, j - 1); }
-        else if (j < _gridSize - 1 && _grid[i, j + 1] == 0) { SwapCells(i, j, i, j + 1); }
-        if (CheckWin()) OnWin?.Invoke();
-    }
-
-    public int GetCellValue(int i, int j)
-    {
-        return _grid[i, j];
-    }
-
-    private void SwapCells(int i, int j, int ni, int nj)
-    {
-        (_grid[ni, nj], _grid[i, j]) = (_grid[i, j], _grid[ni, nj]);
-        OnCellSwapped?.Invoke(i, j, ni, nj);
-    }
-
-    private void InitializeGrid()
-    {
-        for (int i = 0; i < _gridSize; i++)
-        {
-            for (int j = 0; j < _gridSize; j++)
+            if (grid == null)
             {
-                _grid[i, j] = j + (i * _gridSize);
+                _grid = new int[gridSize, gridSize];
+                _gridSize = gridSize;
+                InitializeGrid();
             }
-        }
-        do
-        {
-            System.Random rng = new System.Random();
-
-            for (int i = _gridSize * _gridSize - 1; i > 0; i--)
+            else
             {
-                int j = rng.Next(i + 1);
-
-                int currentRow = i / _gridSize;
-                int currentCol = i % _gridSize;
-                int randomRow = j / _gridSize;
-                int randomCol = j % _gridSize;
-                    
-                (_grid[randomRow, randomCol], _grid[currentRow, currentCol]) = (_grid[currentRow, currentCol], _grid[randomRow, randomCol]);
+                _grid = (int[,])grid.Clone();
+                _gridSize = gridSize;
+                CheckGridValid();
             }
-        }
-        while (CheckWin());
-    }
 
-    private bool CheckWin()
-    {
-        for(int i = _gridSize - 1; i > 0; i--)
+        }
+
+        public void ClickOnCell(int i, int j)
         {
-            for(int j = _gridSize - 1; j > 0; j--)
-            {
-                if (i == _gridSize - 1) j--;
-                if (_grid[i, j] != j + (i * _gridSize) + 1) return false;
-            }
+            if (i > 0 && _grid[i - 1, j] == 0) { SwapCells(i, j, i - 1, j); }
+            else if (i < _gridSize - 1 && _grid[i + 1, j] == 0) { SwapCells(i, j, i + 1, j); }
+            else if (j > 0 && _grid[i, j - 1] == 0) { SwapCells(i, j, i, j - 1); }
+            else if (j < _gridSize - 1 && _grid[i, j + 1] == 0) { SwapCells(i, j, i, j + 1); }
+            if (CheckWin()) OnWin?.Invoke();
         }
-        return true;
-    }
 
-    private void CheckGridValid()
-    {
-
-    }
-
-    private void DebugGrid()
-    {
-        StringBuilder result = new StringBuilder("");
-        for (int i = 0; i < _gridSize; i++)
+        public int GetCellValue(int i, int j)
         {
-            for (int j = 0; j < _gridSize; j++)
-            {
-                result.Append(GetCellValue(i, j) + " ");
-            }
-            result.Append("\n");
+            return _grid[i, j];
         }
-        Debug.Log(result.ToString());
+
+        private void SwapCells(int i, int j, int ni, int nj)
+        {
+            (_grid[ni, nj], _grid[i, j]) = (_grid[i, j], _grid[ni, nj]);
+            OnCellSwapped?.Invoke(i, j, ni, nj);
+        }
+
+        private void InitializeGrid()
+        {
+            for (int i = 0; i < _gridSize; i++)
+            {
+                for (int j = 0; j < _gridSize; j++)
+                {
+                    _grid[i, j] = j + (i * _gridSize);
+                }
+            }
+            do
+            {
+                System.Random rng = new System.Random();
+
+                for (int i = _gridSize * _gridSize - 1; i > 0; i--)
+                {
+                    int j = rng.Next(i + 1);
+
+                    int currentRow = i / _gridSize;
+                    int currentCol = i % _gridSize;
+                    int randomRow = j / _gridSize;
+                    int randomCol = j % _gridSize;
+
+                    (_grid[randomRow, randomCol], _grid[currentRow, currentCol]) = (_grid[currentRow, currentCol], _grid[randomRow, randomCol]);
+                }
+            }
+            while (CheckWin());
+        }
+
+        private bool CheckWin()
+        {
+            for (int i = _gridSize - 1; i > 0; i--)
+            {
+                for (int j = _gridSize - 1; j > 0; j--)
+                {
+                    if (i == _gridSize - 1) j--;
+                    if (_grid[i, j] != j + (i * _gridSize) + 1) return false;
+                }
+            }
+            return true;
+        }
+
+        private void CheckGridValid()
+        {
+
+        }
+
+        private void DebugGrid()
+        {
+            StringBuilder result = new StringBuilder("");
+            for (int i = 0; i < _gridSize; i++)
+            {
+                for (int j = 0; j < _gridSize; j++)
+                {
+                    result.Append(GetCellValue(i, j) + " ");
+                }
+                result.Append("\n");
+            }
+            Debug.Log(result.ToString());
+        }
     }
 }
--- Assets/Puzzle15/Tests/Editor/GameControllerTest.cs
+++ Assets/Puzzle15/Tests/Editor/GameControllerTest.cs
@@ -1,163 +1,216 @@
 using NUnit.Framework;
-using UnityEngine;
+using Puzzle15;
 
-public class GameControllerTest
+namespace Puzzle15Tests
 {
-    [Test]
-    public void GetCellValueTest()
+    public class GameControllerTest
     {
-        //
-        int gridSize = 4;
-        int[,] grid = new int[gridSize, gridSize];
-
-        for (int i = 0; i < gridSize; i++)
+        [Test]
+        public void WhenGetCellValue_AndGridSize4_ThenValueIsCorrect()
         {
-            for (int j = 0; j < gridSize; j++)
-            {
-                grid[i, j] = j + (i * gridSize);
-            }
-        }
-        GameController gameController = new(gridSize, grid);
+            // Arrange
+            int gridSize = 4;
+            int[,] grid = new int[gridSize, gridSize];
 
-        //
-        bool isCorrect = true;
-        for (int i = 0; i < gridSize; i++)
-        {
-            for (int j = 0; j < gridSize; j++)
+            for (int i = 0; i < gridSize; i++)
             {
-                if (gameController.GetCellValue(i, j) != grid[i, j])
+                for (int j = 0; j < gridSize; j++)
                 {
-                    isCorrect = false;
+                    grid[i, j] = j + (i * gridSize);
                 }
             }
+            GameController gameController = new(gridSize, grid);
+
+            // Act
+            bool isCorrect = true;
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    if (gameController.GetCellValue(i, j) != grid[i, j])
+                    {
+                        isCorrect = false;
+                    }
+                }
+            }
+
+            // Assert
+            Assert.True(isCorrect);
         }
 
-        //
-        Assert.True(isCorrect);
-    }
-
-    private bool isWin;
-    [Test]
-    public void WinTest_GridSize_3()
-    {
-        //
-        isWin = false;
-        int gridSize = 3;
-
-        GameController gameController = new(gridSize, GenerateGridForWinTest(gridSize));
-        gameController.OnWin += SetWin;
-
-        //
-        gameController.ClickOnCell(2, 2);
-        gameController.OnWin -= SetWin;
-
-        //
-        Assert.True(isWin);
-    }
-
-    [Test]
-    public void WinTest_GridSize_4()
-    {
-        //
-        isWin = false;
-        int gridSize = 4;
-
-        GameController gameController = new(gridSize, GenerateGridForWinTest(gridSize));
-        gameController.OnWin += SetWin;
-
-        //
-        gameController.ClickOnCell(3, 3);
-        gameController.OnWin -= SetWin;
-
-        //
-        Assert.True(isWin);
-    }
-
-    [Test]
-    public void WinTest_GridSize_5()
-    {
-        //
-        isWin = false;
-        int gridSize = 5;
-
-        GameController gameController = new(gridSize, GenerateGridForWinTest(gridSize));
-        gameController.OnWin += SetWin;
-
-        //
-        gameController.ClickOnCell(4, 4);
-        gameController.OnWin -= SetWin;
-
-        //
-        Assert.True(isWin);
-    }
-
-    [Test]
-    public void ClickOnCellBoundariesTest_LeftUp()
-    {
-        //
-        int gridSize = 4;
-        int[,] grid =
+        private bool isWin;
+        [Test]
+        public void WhenClickOnCell_AndSubscribeOnWin_GridSize3_ThenIsWinInvoked()
         {
+            // Arrange
+            isWin = false;
+            int gridSize = 3;
+
+            GameController gameController = new(gridSize, GenerateGridForWinTest(gridSize));
+            gameController.OnWin += SetWin;
+
+            // Act
+            gameController.ClickOnCell(2, 2);
+            gameController.OnWin -= SetWin;
+
+            // Assert
+            Assert.True(isWin);
+        }
+
+        [Test]
+        public void WhenClickOnCell_AndSubscribeOnWin_GridSize4_ThenIsWinInvoked()
+        {
+            // Arrange
+            isWin = false;
+            int gridSize = 4;
+
+            GameController gameController = new(gridSize, GenerateGridForWinTest(gridSize));
+            gameController.OnWin += SetWin;
+
+            // Act
+            gameController.ClickOnCell(3, 3);
+            gameController.OnWin -= SetWin;
+
+            // Assert
+            Assert.True(isWin);
+        }
+
+        [Test]
+        public void WhenClickOnCell_AndSubscribeOnWin_GridSize5_ThenIsWinInvoked()
+        {
+            // Arrange
+            isWin = false;
+            int gridSize = 5;
+
+            GameController gameController = new(gridSize, GenerateGridForWinTest(gridSize));
+            gameController.OnWin += SetWin;
+
+            // Act
+            gameController.ClickOnCell(4, 4);
+            gameController.OnWin -= SetWin;
+
+            // Assert
+            Assert.True(isWin);
+        }
+
+        [Test]
+        public void WhenClickOnBoundaryCell_AndSavePrevValues_ThenLeftUpValueSwapped()
+        {
+            // Arrange
+            int gridSize = 4;
+            int[,] grid =
+            {
             { 1, 0, 2, 3 },
             { 4, 5, 6, 7 },
             { 8, 9, 10, 11 },
-            { 12, 13, 14, 15 } 
+            { 12, 13, 14, 15 }
         };
-        GameController gameController = new(gridSize, grid);
+            GameController gameController = new(gridSize, grid);
 
-        int prev1 = gameController.GetCellValue(0, 0);
-        int prev2 = gameController.GetCellValue(0, 1);
+            int prev1 = gameController.GetCellValue(0, 0);
+            int prev2 = gameController.GetCellValue(0, 1);
 
-        //
-        gameController.ClickOnCell(0, 0);
+            // Act
+            gameController.ClickOnCell(0, 0);
 
-        //
-        Assert.AreEqual(prev1, gameController.GetCellValue(0, 1));
-        Assert.AreEqual(prev2, gameController.GetCellValue(0, 0));
-    }
+            // Assert
+            Assert.AreEqual(prev1, gameController.GetCellValue(0, 1));
+            Assert.AreEqual(prev2, gameController.GetCellValue(0, 0));
+        }
 
-    [Test]
-    public void ClickOnCellBoundariesTest_LeftDown()
-    {
-        //
-        int gridSize = 4;
-        int[,] grid =
+        [Test]
+        public void WhenClickOnBoundaryCell_AndSavePrevValues_ThenLeftDownValueSwapped()
         {
+            // Arrange
+            int gridSize = 4;
+            int[,] grid =
+            {
             { 1, 8, 2, 3 },
             { 4, 5, 6, 7 },
             { 13, 9, 10, 11 },
             { 12, 0, 14, 15 }
         };
-        GameController gameController = new(gridSize, grid);
+            GameController gameController = new(gridSize, grid);
 
-        int prev1 = gameController.GetCellValue(gridSize - 1, 0);
-        int prev2 = gameController.GetCellValue(gridSize - 1, 1);
+            int prev1 = gameController.GetCellValue(gridSize - 1, 0);
+            int prev2 = gameController.GetCellValue(gridSize - 1, 1);
 
-        //
-        gameController.ClickOnCell(gridSize - 1, 0);
+            // Act
+            gameController.ClickOnCell(gridSize - 1, 0);
 
-        //
-        Assert.AreEqual(prev1, gameController.GetCellValue(gridSize - 1, 1));
-        Assert.AreEqual(prev2, gameController.GetCellValue(gridSize - 1, 0));
-    }
-
-    private int[,] GenerateGridForWinTest(int gridSize)
-    {
-        int[,] grid = new int[gridSize, gridSize];
-        for (int j = 0; j < gridSize; j++)
-        {
-            for (int i = 0; i < gridSize; i++)
-            {
-                
-                grid[i, j] = (i == gridSize - 1) && (j == gridSize - 1) ? 0 : j + (i * gridSize) + 1;
-            }
+            // Assert
+            Assert.AreEqual(prev1, gameController.GetCellValue(gridSize - 1, 1));
+            Assert.AreEqual(prev2, gameController.GetCellValue(gridSize - 1, 0));
         }
-        (grid[gridSize - 1, gridSize - 1], grid[gridSize - 1, gridSize - 2]) = (grid[gridSize - 1, gridSize - 2], grid[gridSize - 1, gridSize - 1]);
-        return grid;
-    }
 
-    private void SetWin()
-    {
-        isWin = true;
+        [Test]
+        public void WhenClickOnBoundaryCell_AndSavePrevValues_ThenRightUpValueSwapped()
+        {
+            // Arrange
+            int gridSize = 4;
+            int[,] grid =
+            {
+            { 1, 8, 0, 3 },
+            { 4, 5, 6, 7 },
+            { 13, 9, 10, 11 },
+            { 12, 2, 14, 15 }
+        };
+            GameController gameController = new(gridSize, grid);
+
+            int prev1 = gameController.GetCellValue(0, gridSize - 1);
+            int prev2 = gameController.GetCellValue(0, gridSize - 2);
+
+            // Act
+            gameController.ClickOnCell(0, gridSize - 1);
+
+            // Assert
+            Assert.AreEqual(prev1, gameController.GetCellValue(0, gridSize - 2));
+            Assert.AreEqual(prev2, gameController.GetCellValue(0, gridSize - 1));
+        }
+
+        [Test]
+        public void WhenClickOnBoundaryCell_AndSavePrevValues_ThenRightDownValueSwapped()
+        {
+            // Arrange
+            int gridSize = 4;
+            int[,] grid =
+            {
+            { 1, 8, 14, 3 },
+            { 4, 5, 6, 7 },
+            { 13, 9, 10, 11 },
+            { 12, 2, 0, 15 }
+        };
+            GameController gameController = new(gridSize, grid);
+
+            int prev1 = gameController.GetCellValue(gridSize - 1, gridSize - 1);
+            int prev2 = gameController.GetCellValue(gridSize - 1, gridSize - 2);
+
+            // Act
+            gameController.ClickOnCell(gridSize - 1, gridSize - 1);
+
+            // Assert
+            Assert.AreEqual(prev1, gameController.GetCellValue(gridSize - 1, gridSize - 2));
+            Assert.AreEqual(prev2, gameController.GetCellValue(gridSize - 1, gridSize - 1));
+        }
+
+        private int[,] GenerateGridForWinTest(int gridSize)
+        {
+            int[,] grid = new int[gridSize, gridSize];
+            for (int j = 0; j < gridSize; j++)
+            {
+                for (int i = 0; i < gridSize; i++)
+                {
+
+                    grid[i, j] = (i == gridSize - 1) && (j == gridSize - 1) ? 0 : j + (i * gridSize) + 1;
+                }
+            }
+            (grid[gridSize - 1, gridSize - 1], grid[gridSize - 1, gridSize - 2]) = (grid[gridSize - 1, gridSize - 2], grid[gridSize - 1, gridSize - 1]);
+            return grid;
+        }
+
+        private void SetWin()
+        {
+            isWin = true;
+        }
     }
 }
